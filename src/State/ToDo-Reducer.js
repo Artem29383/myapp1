614947625519b8814ae621a248c93df81973276a
Artem29383 <54779368+Artem29383@users.noreply.github.com>
2019-11-30 01:31:04 +0300

    Update ToDo-Reducer.js

--- src/State/ToDo-Reducer.js
+++ src/State/ToDo-Reducer.js
@@ -1,49 +1,54 @@
 import { getStorage } from "../Utils/LocalStorage";
-
-const ADD_TASK = 'ToDo-Reducer/ADD_TASK';
-const CHANGE_TASK_STATUS = 'ToDo-Reducer/CHANGE__CHECK';
-const REMOVE_TASK = 'ToDo-Reducer/REMOVE_TASK';
-const SELECT_ALL_TASK = 'ToDo-Reducer/SELECT_ALL_TASK';
-const CONTROL_ALL_SELECTED = 'ToDo-Reducer/CONTROL_ALL_SELECTED';
-const LEFT_TASKS = 'ToDo-Reducer/LEFT_TASKS';
-const REMOVE_SELECT_TASKS = 'ToDo-Reducer/REMOVE_SELECT_TASKS';
-const END_EDIT_TASK = 'ToDo-Reducer/END_EDIT_TASK';
-const REMOVE_EMPTY_TASK = 'ToDo-Reducer/REMOVE_EMPTY_TASK';
-const FILTER_TASKS =  'ToDo-Reducer/FILTER_TASKS';
-
+import {
+  ADD_TASK,
+  CHANGE_TASK_STATUS,
+  CONTROL_ALL_SELECTED, END_EDIT_TASK, FILTER_TASKS, LEFT_TASKS, REMOVE_EMPTY_TASK,
+  REMOVE_SELECT_TASKS,
+  REMOVE_TASK,
+  SELECT_ALL_TASK
+} from "./ActionConst";
 
 let initialState = {
   tasks: getStorage('todo'),
   allSelected: false,
   leftTasks: 0,
-  filter: getStorage('filter', 'All')
+  filter: getStorage('filter', 'All'),
 };
+
+/*
+const task = new schema.Entity('task');
+const tasksSchema = {tasks: [task]};
+const normalizedData = normalize(initialState, tasksSchema);
+initialState = {
+  tasks: {
+    entities: normalizedData.entities.tasks || [],
+    ids: normalizedData.result.tasks || []
+  },
+  allSelected: false,
+  leftTasks: 0,
+  filter: getStorage('filter', 'All'),
+};
+*/
 
 
 const taskReducer = (state = initialState, action) => {
+  localStorage.setItem('todo', JSON.stringify(state.tasks));
+  localStorage.setItem('filter', JSON.stringify(state.filter));
   switch (action.type) {
     case ADD_TASK:
-      if (action.valueTask.trim() !== '') {
-        return {
+      return {
           ...state,
-          tasks: [...state.tasks, {id: action.taskId, check: false, title: action.valueTask}],
-        };
-      }
+          tasks: [...state.tasks, {id: action.payload[0], check: false, title: action.payload[1]}],
+      };
     case CHANGE_TASK_STATUS:
       return {
         ...state,
-        tasks: state.tasks.map(t => {
-          if (t.id === action.taskId) {
-            return {...t, check: !t.check}
-          } else {
-            return {...t, check: t.check}
-          }
-        })
+        tasks: action.payload
       };
     case REMOVE_TASK:
       return {
         ...state,
-        tasks: state.tasks.filter(t => t.id !== action.taskId)
+        tasks: state.tasks.filter(t => t.id !== action.payload)
       };
     case REMOVE_SELECT_TASKS:
       return {
@@ -63,43 +68,27 @@ const taskReducer = (state = initialState, action) => {
     case LEFT_TASKS:
       return {
         ...state,
-        leftTasks: state.tasks.filter(t => !t.check).length,
+        leftTasks: action.payload
       };
     case END_EDIT_TASK:
       return {
         ...state,
-        tasks: state.tasks.map(t => {
-          if (t.id === action.taskId) {
-            return {...t, title: action.message}
-          } else {
-            return {...t}
-          }
-        })
+        tasks: action.payload
       };
     case REMOVE_EMPTY_TASK:
       return {
         ...state,
-        tasks: state.tasks.filter(t => t.title !== '')
+        tasks: action.payload
       };
     case FILTER_TASKS:
       return {
         ...state,
-        filter: action.filterMethod
+        filter: action.payload
       };
     default:
       return state;
   }
 };
 
-  export const addTask = (taskId, valueTask) => ({type: ADD_TASK, taskId, valueTask});
-  export const changeCheck = (taskId) => ({type: CHANGE_TASK_STATUS, taskId});
-  export const removeTask = (taskId) => ({type: REMOVE_TASK, taskId});
-  export const selectedAllTasks = () => ({type: SELECT_ALL_TASK});
-  export const controllAllSelected = () => ({type: CONTROL_ALL_SELECTED});
-  export const getCountLeftTasks = () => ({type: LEFT_TASKS});
-  export const removeSelectedTasks = () => ({type: REMOVE_SELECT_TASKS});
-  export const endEditTask = (taskId, message) => ({type: END_EDIT_TASK, taskId, message});
-  export const removeEmptyTask = () => ({type: REMOVE_EMPTY_TASK});
-  export const filterTasks = (filterMethod) => ({type: FILTER_TASKS, filterMethod});
 
-  export default taskReducer;
+export default taskReducer;
